c136586d73bb29d5f5d4e2dfca238e71139427bb
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-01-25 12:16:43 +0545

    add scripts

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# python_pangeo_cmip6
-Python scripts to download CMIP6 data from Pangeo Gallery
+# Python scripts accessing data Pangeo CMIP6 Gallery
+
+A python script to download cmip6 data from Pangeo and Google cloud storage for given scenarios and models. The original data is available in Pangeo CMIP6 Gallery at http://gallery.pangeo.io/repos/pangeo-gallery/cmip6/index.html.
+
+# Requirements:
+
+Requires installation of several python packages. The packages can be installed through a conda installation. Create a new environment from the yml included in the repository as
+
+```conda env create --name pg_cmip6 --file environment_macos.yml``` for mac
+
+```conda env create --name pg_cmip6 --file environment_linux.yml``` for linux, cluster (e.g., atacama)
+
+The environment can be activated using
+
+```conda activate pg_cmip6```
+
+# Usage:
+Run the python script with the input json filename as the argument.
+
+```python get_cmip6.py input.json```
+
+# Options and Settings
+
+The input json includes the following fields.
+
+- metadata_csv: Path to the metadata file for the google data storage. A locally downloaded csv can be used. If empty (""), it will automatically retrieve the metadata from https://storage.googleapis.com/cmip6/cmip6-zarr-consolidated-stores.csv.
+
+- out_dir: Path to the output directory.
+
+- experiments: The experiments for which the data should be downloaded. Each experiment has a list input as [start year, end year] for the period of the data to be downloaded, e.g.,
+
+```json
+    historical: [
+        1980,
+        2010
+    ]
+```
+
+- variables: The variables to be downloaded in the format ```variable_name: table ID```, e.g., "tas": "Amon".
+
+- target_grid: A setting for the taget spatial resolution as [lat_reso, lon_reso], e.g., [1, 1] to regrid all data to 1 degree by 1 degree spatial resolution. If the list is blank [], the data will not be regridded, and only the original data will be saved.
+
+- regrid_method: The method to be used for spatial regridding based on xESMF (https://xesmf.readthedocs.io/en/latest/index.html). Use one of ```['bilinear', 'conservative', 'nearest_s2d', 'nearest_d2s', 'patch']```.
+
+- download_all: Nuclear option to download all available models and ensemble members for the given variables and experiments. When set to false, only the models and ensemble members given in the data_set section will be downloaded. Naturally, setting download_all to true will be slower than downloading selected data.
+
+- dataset: A list of models and ensembles to be downloaded given as a dictionary block for each model. The key is an unique identifier. source_id is the exact model name, ens_members is a list of ensemble members. Add a block per model for multiple models.
+
+```json
+    ipsl: {
+        source_id: IPSL-CM6A-LR,
+        ens_members: [
+            r1i1p1f1
+        ]
+    }
+```
+
+# Disclaimer:
+
+This repository is created to support the download and regriding CMIP6 data for research purposes. Any usage beyond the intended purpose are the responsibility of the users.
+
+The developer does not bear any responsibility for the data quality and correctness. All users should also cite the original data accordingly, and acknowledge the usage of this tool for usage as:
+
+
+
+
+# License: 
+
+Attribution 4.0 International (CC BY 4.0).
+
+# Useful Links
+
+- http://gallery.pangeo.io/repos/pangeo-gallery/cmip6/basic_search_and_load.html
---
+++ get_cmip6.py
@@ -0,0 +1,114 @@
+# A script to use pangeo data store to download cmip6 data_set
+# Developer: Sujan Koirala (skoirala@bgc-jena.mpg.de)
+# Date: 2021-02-09
+import pandas as pd
+import fsspec
+import xarray as xr
+import xesmf as xe
+import matplotlib.pyplot as plt
+import json
+import os
+import numpy as np
+import sys
+
+# load the input settings
+in_settings = sys.argv[1]
+input = json.load(open(in_settings))
+
+# load metadata of the data in pangeo
+if len(input['metadata_csv'].strip()) > 0:
+    meta_data = pd.read_csv(input['metadata_csv'])
+    print("using local metadta from: ", input['metadata_csv'])
+else:
+    print("using metadta from https://storage.googleapis.com/cmip6/cmip6-zarr-consolidated-stores.csv")
+    meta_data = pd.read_csv('https://storage.googleapis.com/cmip6/cmip6-zarr-consolidated-stores.csv')
+
+# prepare the output directory
+out_dir = input['out_dir']
+os.makedirs(out_dir, exist_ok=True)
+data_set=input['dataset']
+
+# check if the download all option is invoked. If True, all unique models and ensemble members are obtained from metadata
+if input['download_all']:
+    models = meta_data['source_id'].unique()
+    ens_members = meta_data['member_id'].unique()
+    print (models, ens_members)
+    full_ensemble = True
+else:
+    models = data_set.keys()
+    ens_members = None
+    full_ensemble = False
+
+# check if regrid option is activated and create a lat lon array for target spatial grids
+if len(input['target_grid']) == 2:
+    regrid_data = True
+    tar_lat_int = input['target_grid'][0]
+    tar_lon_int = input['target_grid'][1]
+    n_lat = int(180./tar_lat_int)
+    n_lon = int(360./tar_lon_int)
+    lat_min = -90 + tar_lat_int/2.
+    lat_max = 90 - tar_lat_int/2.
+    lon_max = 360 - tar_lon_int/2.
+    lon_min = tar_lon_int/2.
+    new_lat = np.linspace(lat_min, lat_max, n_lat, endpoint=True)
+    new_lon = np.linspace(lon_min, lon_max, n_lon, endpoint=True)
+else:
+    regrid_data = False
+
+# loop through the experiments and dataset
+for experiment, info in input['experiments'].items():
+    start_year = info[0]
+    end_year = info[1]
+    # loop through the unique models
+    for model in models:
+        # get ensemble members if download_all is False
+        if not full_ensemble:
+            ens_members = data_set[model]['ens_members']
+            src = data_set[model]['source_id']
+        else:
+            src = model
+        # loop through the variables
+        for variable, table in input['variables'].items():
+            # loop through the ensemble
+            for variant in ens_members:
+                qry = "table_id == '" + table +"' & variable_id == '" + variable + "' & experiment_id == '" + experiment + "' & source_id == '" + src + "' & member_id == '" + variant + "'"
+                meta_data_sel = meta_data.query(qry)
+                if not meta_data_sel.empty:
+                    # print(meta_data_sel)
+                    zstore = meta_data_sel.zstore.values[-1]
+                    # create a mutable-mapping-style interface to the store
+                    mapper = fsspec.get_mapper(zstore)
+                    # open it using xarray and zarr
+                    ds = xr.open_zarr(mapper, consolidated=True)
+                    ds_sel_time = ds.sel(time=slice(str(start_year), str(end_year)))
+
+
+                    file_name_nc = '{var}_{exp}_{tab}_{sroc}_{vrnt}_{syr}_{eyr}.nc'.format(var=variable, exp=experiment, tab=table, sroc=src, vrnt=variant, syr=str(start_year), eyr=end_year)
+                    out_file = os.path.join(out_dir, file_name_nc)
+                    ds_sel_time.to_netcdf(out_file)
+                    print('saved original resolution: ', out_file)
+                    # regrid if the option is invoked
+                    if regrid_data:
+                        # create dataset with target grid
+                        ds_out = xr.Dataset({'lat': (['lat'], new_lat), 'lon': (['lon'], new_lon),})
+                        # create regridder object
+                        regridder = xe.Regridder(ds_sel_time, ds_out, input['regrid_method'])
+                        # regrid data
+                        ds_int = regridder(ds_sel_time)
+                        # create and save the output
+                        file_name_nc_int = '{var}_{exp}_{tab}_{sroc}_{vrnt}_{syr}_{eyr}_{t_lat}x{t_lon}.nc'.format(var=variable, exp=experiment, tab=table, sroc=src, vrnt=variant, syr=str(start_year), eyr=end_year, t_lat=str(tar_lat_int), t_lon=str(tar_lon_int))
+                        out_file_int = os.path.join(out_dir, file_name_nc_int)
+                        ds_int.to_netcdf(out_file_int)
+                        # close dataset
+                        ds_int.close()
+                        ds_out.close()
+                        print('saved regridded data: ', out_file)
+                    # close dataset
+                    ds_sel_time.close()
+                    ds.close()
+                else:
+                    print('Data not found for:')
+                    print(qry)
+                print ('---------------------------------------')
+            
+
